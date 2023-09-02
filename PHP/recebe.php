5b2bf8f4ca012cd90165a1f2f33541cd5e68e275
Thiago0808 <thiago.mo08@aluno.ifsc.edu.br>
2023-09-02 13:05:47 -0300

    Tentativa de Arrumar DataHora

--- PHP/recebe.php
+++ PHP/recebe.php
@@ -2,16 +2,14 @@
 require 'conexao.php';
 $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
 $texto = filter_input(INPUT_POST, 'texto', FILTER_SANITIZE_SPECIAL_CHARS);
-$data = date_default_timezone_set('America/Sao_Paulo');
 $cor = filter_input(INPUT_POST, 'cor', FILTER_SANITIZE_SPECIAL_CHARS);
 
-date_default_timezone_set('America/Sao_Paulo');
+//DATA E HORA ESTAO COM PROBLEMA
+//date_default_timezone_set('America/Sao_Paulo');
+//$data = date('Y-m-d H:i:s');
+$conexao->query("INSERT INTO comentario (texto, nome, cor) VALUES ('$texto', '$nome', '$cor')");
 
-$data = date("Y-m-d H:i:s");  
-$datahora=date("Y-m-d",strtotime($data));
-
-
-$conexao->query("INSERT INTO comentario (texto, datahora, nome, cor) VALUES ('$texto', $datahora, '$nome', '$cor')");
+//$conexao->query("INSERT INTO comentario (texto, dataHora, nome, cor) VALUES ('$texto', $data, '$nome', '$cor')");
 
 #redireciona
 header('Location: ../interface/chat.php');
