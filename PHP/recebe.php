899976b0ce43cdede38859898ce63532663f68ec
Thiago Martins <thiago.mo08@aluno.ifsc.edu.br>
2023-09-06 13:48:07 -0300

    DataHora Consertada

--- PHP/recebe.php
+++ PHP/recebe.php
@@ -5,11 +5,12 @@ $texto = filter_input(INPUT_POST, 'texto', FILTER_SANITIZE_SPECIAL_CHARS);
 $cor = filter_input(INPUT_POST, 'cor', FILTER_SANITIZE_SPECIAL_CHARS);
 
 //DATA E HORA ESTAO COM PROBLEMA
-//date_default_timezone_set('America/Sao_Paulo');
-//$data = date('Y-m-d H:i:s');
-$conexao->query("INSERT INTO comentario (texto, nome, cor) VALUES ('$texto', '$nome', '$cor')");
+date_default_timezone_set('America/Sao_Paulo');
+$data = date("Y-m-d H:i:s");
 
-//$conexao->query("INSERT INTO comentario (texto, dataHora, nome, cor) VALUES ('$texto', $data, '$nome', '$cor')");
+echo $data;
+
+$conexao->query("INSERT INTO comentario (texto, dataHora, nome, cor) VALUES ('$texto', '$data', '$nome', '$cor')");
 
 #redireciona
 header('Location: ../interface/chat.php');
