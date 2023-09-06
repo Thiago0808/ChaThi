dac1fd603f330a39e5f3c46459ae1ef5c77db10a
Thiago Martins <thiago.mo08@aluno.ifsc.edu.br>
2023-09-06 16:16:45 -0300

    Verificação + Data Adaptada

--- PHP/recebe.php
+++ PHP/recebe.php
@@ -4,13 +4,11 @@ $nome = filter_input(INPUT_GET, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
 $texto = filter_input(INPUT_GET, 'texto', FILTER_SANITIZE_SPECIAL_CHARS);
 $cor = filter_input(INPUT_GET, 'cor', FILTER_SANITIZE_SPECIAL_CHARS);
 urldecode($cor);
-//DATA E HORA ESTAO COM PROBLEMA
-date_default_timezone_set('America/Sao_Paulo');
-$data = date("Y-m-d H:i");
 
-echo $data;
 
-$conexao->query("INSERT INTO comentario (texto, dataHora, nome, cor) VALUES ('$texto', '$data', '$nome', '$cor')");
+if ($nome && $texto){
+    $conexao->query("INSERT INTO comentario (texto, nome, cor) VALUES ('$texto', '$nome', '$cor')");
+}
 
 
 
