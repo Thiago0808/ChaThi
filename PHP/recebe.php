b543eb8308de599c3d413da3db1bd09e9086a734
Thiago Martins <thiago.mo08@aluno.ifsc.edu.br>
2023-09-06 14:07:13 -0300

    Mudado para GET, Erros consertados

--- PHP/recebe.php
+++ PHP/recebe.php
@@ -1,9 +1,9 @@
 <?php
 require 'conexao.php';
-$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
-$texto = filter_input(INPUT_POST, 'texto', FILTER_SANITIZE_SPECIAL_CHARS);
-$cor = filter_input(INPUT_POST, 'cor', FILTER_SANITIZE_SPECIAL_CHARS);
-
+$nome = filter_input(INPUT_GET, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+$texto = filter_input(INPUT_GET, 'texto', FILTER_SANITIZE_SPECIAL_CHARS);
+$cor = filter_input(INPUT_GET, 'cor', FILTER_SANITIZE_SPECIAL_CHARS);
+urldecode($cor);
 //DATA E HORA ESTAO COM PROBLEMA
 date_default_timezone_set('America/Sao_Paulo');
 $data = date("Y-m-d H:i:s");
@@ -12,8 +12,10 @@ echo $data;
 
 $conexao->query("INSERT INTO comentario (texto, dataHora, nome, cor) VALUES ('$texto', '$data', '$nome', '$cor')");
 
+
+
 #redireciona
-header('Location: ../interface/chat.php');
+header('Location: ../interface/chat.php?nome='.$nome.'&cor='.$cor.' ');
 
 
 
